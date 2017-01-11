479495abcb739b54bc2a446c26f60a3299f40595
CJ <weicongju@gmail.com>
2017-01-11 17:58:29 +0800

    增加 bootstrap.php，修正从php版本7.0到 5.6的支持。

---
+++ src/bootstrap.php
@@ -0,0 +1,63 @@
+<?php
+require_once('vendor/autoload.php');
+
+use Monolog\Logger;
+use DI\Container;
+use DI\ContainerBuilder;
+use DI\ContainerInterface;
+use Symfony\Component\HttpFoundation\Request;
+$log = new Logger('name');
+
+use Doctrine\Common\Annotations\AnnotationReader;
+use Symfony\Component\Validator\Validation;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+
+$reader = new AnnotationReader();
+
+$validator = Validation::createValidatorBuilder()
+    // ->setApiVersion(Validation::API_VERSION_2_5)
+    // ->addXmlMapping(__DIR__.'/validation.xml')
+    ->enableAnnotationMapping($reader)
+    ->getValidator();
+
+class RequestFactory
+{
+    static public function create()
+    {
+        return Request::createFromGlobals();
+    }
+}
+
+$builder = new \DI\ContainerBuilder();
+$builder->setDefinitionCache(new Doctrine\Common\Cache\ArrayCache());
+$builder->writeProxiesToFile(true, __DIR__ . '/../var/cache/ioc');
+$builder->useAnnotations(true);
+$builder->addDefinitions([
+    Request::class => \DI\factory([RequestFactory::class, "create"]),
+    // ValidatorInterface::class, $validator
+]);
+$container = $builder->build();
+
+// Initialize an application aspect container
+$applicationAspectKernel = \core\ApplicationAspectKernel::getInstance();
+$applicationAspectKernel->init(array(
+        'debug' => true, // use 'false' for production mode
+        // Cache directory
+        'cacheDir'  => __DIR__ . '/../var/cache/aspect',
+        // Include paths restricts the directories where aspects should be applied, or empty for all source files
+        'includePaths' => array(
+            __DIR__ . '/src/'
+        )
+));
+
+// echo Request::class;
+$request = $container->get(Request::class);
+// die($request);
+
+
+
+$container->set(
+    ValidatorInterface::class, $validator
+);
+// $container->set(Request::class, \DI\Factory([RequestFactory::class, "create"]));
